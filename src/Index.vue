82dd1fefa7a7caebee89f2184102c657dac13fec
davidosmagnos <davemagno99@gmail.com>
2023-05-08 10:36:20 +0800

    initial commit

---
+++ src/Index.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>henlo</h1>
+</template>
+<script>
+    export default{
+        name:"index-page"
+    }
+</script>
+<style scoped>
+    
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,18 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import Index from "./Index.vue"
 
-createApp(App).mount('#app')
+var routes = [
+    {
+        path:"/",
+        name:"Index",
+        component: Index
+    },
+]
+const router = createRouter({
+    history:createWebHistory(process.env.BASE_URL),
+    routes
+})
+
+createApp(App).use(router).mount('#app')
